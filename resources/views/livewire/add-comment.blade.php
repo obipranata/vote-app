3145b76f1d9323a75e1ffd1a89292126bd948353
obipranata <obiimanuelpranata@gmail.com>
2023-02-28 22:12:56 +0700

    comments ux

--- resources/views/livewire/add-comment.blade.php
+++ resources/views/livewire/add-comment.blade.php
@@ -1,15 +1,31 @@
 <div 
     x-data="{isOpen : false}" 
     x-init = "
-    Livewire.on('commentWasAdded', () => {
-        isOpen = false;
-    })"
+        Livewire.on('commentWasAdded', () => {
+            isOpen = false;
+        })
+        Livewire.hook('message.processed', (message, component) => {
+            if(message.updateQueue[0].payload.event === 'commentWasAdded' && message.component.fingerprint.name === 'idea-comments'){
+                lastComment = document.querySelector('.comment-container:last-child')
+                lastComment.scrollIntoView({behavior: 'smooth'})
+                lastComment.classList.add('bg-green-50')
+                setTimeout(() => {
+                    lastComment.classList.remove('bg-green-50')
+                }, 5000)
+            }
+        })
+    "
     class="relative"
 >
     <button 
-    @click="isOpen = !isOpen"
-    type="button"
-    class="flex items-center justify-center h-11 w-32 text-sm bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
+        @click="
+            isOpen = !isOpen
+            if(isOpen){
+                $nextTick(() => $refs.comment.focus())
+            }
+        "
+        type="button"
+        class="flex items-center justify-center h-11 w-32 text-sm bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
     >
         Reply
     </button>
@@ -23,7 +39,7 @@
     @auth
         <form wire:submit.prevent="addComment" action="#" class="space-y-4 px-4 py-6">
             <div>
-                <textarea wire:model="comment" name="post_comment" id="post_comment" cols="30" rows="4" class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2" placeholder="Go ahead, don't be shy. Share your thoughts..." required></textarea>
+                <textarea x-ref="comment" wire:model="comment" name="post_comment" id="post_comment" cols="30" rows="4" class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2" placeholder="Go ahead, don't be shy. Share your thoughts..." required></textarea>
                 @error('comment')
                     <p class="text-red text-xs mt-1">{{$message}}</p>
                 @enderror
